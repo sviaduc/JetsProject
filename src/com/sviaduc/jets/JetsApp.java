8f2b6064cd091d0b24232478700e827c7bd7e642
Stanton Viaduc <stan@Stantons-MacBook-Pro.local>
2017-08-11 17:06:25 -0600

    added JetsApp class

---
+++ src/com/sviaduc/jets/JetsApp.java
@@ -0,0 +1,14 @@
+package com.sviaduc.jets;
+
+public class JetsApp {
+
+	public JetsApp() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
