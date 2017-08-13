3d3e3c786d761e494814d6bdff0f633863303083
Stanton Viaduc <stan@Stantons-MacBook-Pro.local>
2017-08-13 10:48:42 -0600

    added fleet class

---
+++ src/com/sviaduc/jets/Fleet.java
@@ -0,0 +1,19 @@
+package com.sviaduc.jets;
+
+public class Fleet {
+	private String model;
+	private int speed, range, price;
+	
+	public Fleet(String model, int speed, int range, int price) {
+		super();
+		this.model = model;
+		this.speed = speed;
+		this.range = range;
+		this.price = price;
+	}
+	
+	
+
+	
+
+}
--- src/com/sviaduc/jets/JetsApp.java
+++ src/com/sviaduc/jets/JetsApp.java
@@ -1,14 +1,42 @@
 package com.sviaduc.jets;
 
+import java.util.*;
+
 public class JetsApp {
-
-	public JetsApp() {
-		// TODO Auto-generated constructor stub
-	}
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner kb = new Scanner(System.in);
+		
+		while(true) {
+			
+			System.out.println("Please choose from the following options.");
+			System.out.println("(1) List fleet");
+			System.out.println("(2) View fastest jets");
+			System.out.println("(3) View jet with longest range");
+			System.out.println("(4) Add a jet to fleet");
+			System.out.println("(5) Quit");
+			
+			int menuOption = kb.nextInt();
+			if(menuOption == 5)
+				break;
+			
+			switch (menuOption) {
+			case 1: listFleet();
+				break;
+				
+			case 2: viewFastestJets();
+			break;
+			
+			case 3: viewJetRange();
+			break;
+			
+			case 4: addJet();
+			break;
 
+			default:
+				break;
+			}
+		}
+		
 	}
 
 }
